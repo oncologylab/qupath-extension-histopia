73f097d15380a2c9b2254164320ea60be0ae7530
Yaoxiang Li <liyaoxiang@outlook.com>
2026-07-26 22:49:56 -0400

    Pin bounded semantic normalization

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 qupathExtension {
     name = "qupath-extension-histopia"
     group = "org.oncologylab"
-    version = "0.3.29"
+    version = "0.3.30"
     description = "Project-driven Histopia registration and semantic atlas workflows"
     automaticModule = "org.oncologylab.qupath.extension.histopia"
 }
